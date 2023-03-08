25400fa14e0eed77571fe70ea1f0e81b86d57eef
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-03-08 21:51:43 +0530

    Update sumOfSeries(2).py

--- sumOfSeries(2).py
+++ sumOfSeries(2).py
@@ -2,9 +2,11 @@
 # 1 + 2/2! + 3/3! + 4/4! + ...... + n/n!
 
 def fact(n) :
+    num = 1
     
     if(n >= 1) :
-        return(1 + (n / (n * fact(n-1))))
+        num += 1
+        return(1 + (num / (n * fact(n-1))))
     
     else :
         return 1

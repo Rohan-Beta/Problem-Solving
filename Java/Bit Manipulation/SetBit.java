1125b548fc94dedaf4fe04e0cb98bd87b09d8eca
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-11-23 13:06:03 +0530

    Update SetBit.java

--- Java/Bit Manipulation/SetBit.java
+++ Java/Bit Manipulation/SetBit.java
@@ -7,7 +7,7 @@ class Firstclass {
     public static void main(String args[]) {
 
         int n = 5; // 0101
-        int pos = 1; // 0001
+        int pos = 1;
 
         int bitmask = 1 << pos;
         int newNum = bitmask | n;

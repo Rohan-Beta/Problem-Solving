02b9dfbf9ddb695c611074c7b1b3c166b37d73a1
Beta <125117740+Rohan-Beta@users.noreply.github.com>
2023-11-28 16:41:31 +0530

    Update prime or not.py

--- Python/prime or not.py
+++ Python/prime or not.py
@@ -1,23 +1,18 @@
-# find that the sqare root of the number is prime or not
-
-import math
-
 n = int(input("Enter a number: "))
-num = int(math.sqrt(n))
 
 flag = 0
 
-if(n == 1):
-    print("no")
+if(n == 1 or n == 0) :
+    print("not a prime number")
     
-elif(n > 1):
-    for i in range(2 , n):
+elif(n > 1) :
+    for i in range(2 , n) :
         if(n % i == 0):
-            flag = 1
             
+            flag = 1
             break
-        
-    if(flag):
-        print("no")
-    else:
-        print("yes")
+
+    if(flag == 1) :
+        print("not a prime number")
+    else :
+        print("prime number")
